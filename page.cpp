26f85a9ea38ff0e4a140da84c2d71f0136935670
lhw0841 <58808565+lhw0841@users.noreply.github.com>
2023-06-08 21:48:04 +0900

    Add files via upload

--- page.cpp
+++ page.cpp
@@ -1,133 +1,99 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <fstream>
+#include <queue>
+#include <stack>
 #include <vector>
-#include <cstring>
+#include <algorithm>
+#include <list>
+
 using namespace std;
 
 int main() {
-    ifstream fin("page.inp");
-    ofstream fout("page.out");
+    ifstream in("page.inp");
+    ofstream out("page.out");
 
-    int n;
-    fin >> n;
+    int frameSize;
+    in >> frameSize;
 
-    vector<int> ref;
-    while (1) {
-        int x;
-        fin >> x;
-        if (x == -1) break;
-        ref.push_back(x);
+    vector<int> referenceString;
+    int reference;
+    while (in >> reference) {
+        if (reference == -1) break;
+        referenceString.push_back(reference);
     }
 
-    int len = ref.size();
-    int* fifo = new int[n];
-    int* lru = new int[n];
-    int* opt = new int[n];
-    memset(fifo, -1, sizeof(int) * n);
-    memset(lru, -1, sizeof(int) * n);
-    memset(opt, -1, sizeof(int) * n);
-    int fifo_cnt = 0, lru_cnt = 0, opt_cnt = 0;
+    // FIFO
+    queue<int> fifoQueue;
+    vector<bool> fifoPresent(10001, false);
+    int fifoPageFaults = 0;
+    for (int i = 0; i < referenceString.size(); i++) {
+        if (!fifoPresent[referenceString[i]]) {
+            fifoPageFaults++;
+            if (fifoQueue.size() == frameSize) {
+                int removed = fifoQueue.front();
+                fifoQueue.pop();
+                fifoPresent[removed] = false;
+            }
+            fifoQueue.push(referenceString[i]);
+            fifoPresent[referenceString[i]] = true;
+        }
+    }
+    out << "FIFO: " << fifoPageFaults << endl;
 
-    for (int i = 0; i < len; i++) {
-        int x = ref[i];
-        bool flag;
+    // LRU
+    list<int> lruList;
+    vector<bool> lruPresent(10001, false);
+    int lruPageFaults = 0;
+    for (int i = 0; i < referenceString.size(); i++) {
+        if (!lruPresent[referenceString[i]]) {
+            lruPageFaults++;
+            if (lruList.size() == frameSize) {
+                int removed = lruList.back();
+                lruList.pop_back();
+                lruPresent[removed] = false;
+            }
+            lruList.push_front(referenceString[i]);
+            lruPresent[referenceString[i]] = true;
+        }
+        else {
+            lruList.remove(referenceString[i]);
+            lruList.push_front(referenceString[i]);
+        }
+    }
+    out << "LRU: " << lruPageFaults << endl;
 
-        // FIFO
-        flag = false;
-        for (int j = 0; j < n; j++) {
-            if (fifo[j] == x) {
-                flag = true;
+
+    // OPT
+    vector<int> optFrame(frameSize, -1);
+    int optPageFaults = 0;
+    for (int i = 0; i < referenceString.size(); i++) {
+        bool found = false;
+        for (int j = 0; j < frameSize; j++) {
+            if (optFrame[j] == referenceString[i]) {
+                found = true;
                 break;
             }
         }
-        if (!flag) {
-            fifo_cnt++;
-            if (i >= n) {
-                int y = fifo[0];
-                for (int j = 0; j < n - 1; j++) {
-                    fifo[j] = fifo[j + 1];
-                }
-                fifo[n - 1] = x;
-            }
-            else {
-                fifo[i] = x;
-            }
-        }
-
-        // LRU
-        flag = false;
-        for (int j = 0; j < n; j++) {
-            if (lru[j] == x) {
-                flag = true;
-                for (int k = j; k < n - 1; k++) {
-                    lru[k] = lru[k + 1];
-                }
-                lru[n - 1] = x;
-                break;
-            }
-        }
-        if (!flag) {
-            lru_cnt++;
-            if (i >= n) {
-                for (int j = 0; j < n - 1; j++) {
-                    lru[j] = lru[j + 1];
-                }
-                lru[n - 1] = x;
-            }
-            else {
-                lru[i] = x;
-            }
-        }
-
-        // OPT
-        flag = false;
-        for (int j = 0; j < n; j++) {
-            if (opt[j] == x) {
-                flag = true;
-                break;
-            }
-        }
-        if (!flag) {
-            opt_cnt++;
-            int* idx = new int[10001];
-            memset(idx, -1, sizeof(int) * 10001);
-            for (int j = i + 1; j < len; j++) {
-                bool f2 = false;
-                for (int k = 0; k < n; k++) {
-                    if (opt[k] == ref[j]) {
-                        idx[ref[j]] = j;
-                        f2 = true;
+        if (!found) {
+            optPageFaults++;
+            vector<int> nextUse(frameSize, 10001);
+            for (int j = 0; j < frameSize; j++) {
+                for (int k = i + 1; k < referenceString.size(); k++) {
+                    if (referenceString[k] == optFrame[j]) {
+                        nextUse[j] = k;
                         break;
                     }
                 }
-                if (f2) continue;
-
-                for (int k = 0; k < n; k++) {
-                    if (idx[ref[j]] == -1 || idx[ref[j]] > idx[opt[k]]) idx[opt[k]] = idx[ref[j]];
-                }
             }
-
-            int mx_idx = -1, mx_val = -1;
-            for (int j = 0; j < n; j++) {
-                if (idx[opt[j]] > mx_val || mx_idx == -1) {
-                    mx_val = idx[opt[j]];
-                    mx_idx = j;
-                }
-            }
-
-            opt[mx_idx] = x;
-
-            delete[] idx;
+            int maxIndex = max_element(nextUse.begin(), nextUse.end()) - nextUse.begin();
+            optFrame[maxIndex] = referenceString[i];
         }
     }
+    out << "OPT: " << optPageFaults << endl;
 
-    fout << "FIFO: " << fifo_cnt << '\n';
-    fout << "LRU: " << lru_cnt << '\n';
-    fout << "OPT: " << opt_cnt << '\n';
-
-    delete[] fifo;
-    delete[] lru;
-    delete[] opt;
+    in.close();
+    out.close();
 
     return 0;
 }
